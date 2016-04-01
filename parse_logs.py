fc7c9d0921c502a7b63b247ad44194e2303e639f
kkorvel <koit.korvel@gmail.com>
2016-04-01 12:12:26 +0300

    Added files via upload
    uued asjad

--- parse_logs.py
+++ parse_logs.py
@@ -1,79 +1,59 @@
-import os
-import urllib
 import argparse
+import os
+import GeoIP
+from maprender import render_map
+from logparser import LogParser
+
+
+PROJECT_ROOT = os.path.dirname(__file__)
+
 parser = argparse.ArgumentParser(description='Apache2 log parser.')
+parser.add_argument('--output',
+    help="This is where we place the output files such as report.html and map.svg",
+    default='build')
 parser.add_argument('--path',
     help="Path to Apache2 log files", default="/var/log/apache2")
 parser.add_argument('--top-urls',
     help="Find top URL-s", action='store_true')
 parser.add_argument('--geoip',
-    help="Resolve IP-s to country codes", action='store_true') # We'll implement this later ;)
+    help="Resolve IP-s to country codes", default="/usr/share/GeoIP/GeoIP.dat")
 parser.add_argument('--verbose',
     help="Increase verbosity", action="store_true")
+parser.add_argument('--skip-compressed',
+    help="Skip compressed files", action="store_true")
 args = parser.parse_args()
-# Following is the directory with log files,
-# On Windows substitute it where you downloaded the files
-root = "/home/kkorvel/Documents/logs"
+
 try:
     gi = GeoIP.open(args.geoip, GeoIP.GEOIP_MEMORY_CACHE)
-except:
-    print "Failed to open up GeoIP database, are you sure %s exists?" % args.geoip
-    exit(255)
+except GeoIP.error:
+    print "Failed to open up GeoIP database, it seems %s does not exist!" % os.path.realpath(args.geoip)
+    exit(254)
 
-keywords = "Windows", "Linux", "OS X", "Ubuntu", "Googlebot", "bingbot", "Android", "YandexBot", "facebookexternalhit"
-urls = {}
-user_bytes = {}
-countries = {}
-ip_addresses = {}
-total = 0
 import gzip
-for filename in os.listdir(root):
-    if not filename.startswith("access.log"):
+
+logparser = LogParser(gi, keywords = ("Windows", "Linux", "OS X"))
+
+for filename in os.listdir(args.path):
+    if not filename.startswith("access."):
         continue
+
     if filename.endswith(".gz"):
-        fh = gzip.open(os.path.join(root, filename))
-    else:
-        fh = open(os.path.join(root, filename))
-    print "Parsing:", filename
-    for line in fh:
-        total = total + 1
-        try:
-            source_timestamp, request, response, referrer, _, agent, _ = line.split("\"")
-            method, path, protocol = request.split(" ")
-        except ValueError:
+        if args.skip_compressed:
             continue
+        fh = gzip.open(os.path.join(args.path, filename))
+    else:
+        fh = open(os.path.join(args.path, filename))
 
-        source_ip, _, _, timestamp = source_timestamp.split(" ", 3)
+    if args.verbose:
+        print "Parsing:", filename
 
-        if not ":" in source_ip: # Skip IPv6
-            ip_addresses[source_ip] = ip_addresses.get(source_ip, 0) + 1
-            cc = gi.country_code_by_addr(source_ip)
-            countries[cc] = countries.get(cc, 0) + 1
-        if path == "*": continue # Skip asterisk for path
+    logparser.parse_file(fh)
 
-        _, status_code, content_length, _ = response.split(" ")
-        content_length = int(content_length)
-        path = urllib.unquote(path)
+if not logparser.urls:
+    print "No log entries!"
+    exit(254)
 
-        if path.startswith("/~"):
-            username = path[2:].split("/")[0]
-            try:
-                user_bytes[username] = user_bytes[username] + content_length
-            except:
-                user_bytes[username] = content_length
 
-        try:
-            urls[path] = urls[path] + 1
-        except:
-            urls[path] = 1
-
-        for keyword in keywords:
-            if keyword in agent:
-                try:
-                    d[keyword] = d[keyword] + 1
-                except KeyError:
-                    d[keyword] = 1
-                break
 def humanize(bytes):
     if bytes < 1024:
         return "%d B" % bytes
@@ -83,26 +63,6 @@ def humanize(bytes):
         return "%.1f MB" % (bytes / 1024.0 ** 2)
     else:
         return "%.1f GB" % (bytes / 1024.0 ** 3)
-    from lxml import etree
-from lxml.cssselect import CSSSelector
-
-document =  etree.parse(open(os.path.join(PROJECT_ROOT, 'templates', 'map.svg')))
-
-max_hits = max(countries.values())
-
-for country_code, hits in countries.items():
-    if not country_code: continue
-    print country_code, hex(hits * 255 / max_hits)[2:]
-    sel = CSSSelector("#" + country_code.lower())
-    for j in sel(document):
-
-        j.set("style", "fill:hsl(%d, 90%%, 70%%);" % (120 - hits * 120 / max_hits))
-
-        for i in j.iterfind("{http://www.w3.org/2000/svg}path"):
-            i.attrib.pop("class", "")
-
-with open(os.path.join(args.output, "map.svg"), "w") as fh:
-    fh.write(etree.tostring(document))
 
 from jinja2 import Environment, FileSystemLoader # This it the templating engine we will use
 
@@ -112,29 +72,24 @@ env = Environment(
 
 import codecs
 
+
+rendered_map = render_map(
+    open(os.path.join(PROJECT_ROOT, 'templates', 'map.svg')),
+    logparser.countries)
+
+
+
 context = {
-    "humanize": humanize,
-    "url_hits": sorted(urls.items(), key=lambda i:i[1], reverse=True),
-    "user_bytes": sorted(user_bytes.items(), key = lambda item:item[1], reverse=True),
+    "map_svg": rendered_map,
+    "humanize": humanize, # This is why we use locals() :D
+    "keyword_hits": sorted(logparser.d.items(), key=lambda i:i[1], reverse=True),
+    "url_hits": sorted(logparser.urls.items(), key=lambda i:i[1], reverse=True),
+    "user_bytes": sorted(logparser.user_bytes.items(), key = lambda item:item[1], reverse=True),
 }
 
 with codecs.open(os.path.join(args.output, "report.html"), "w", encoding="utf-8") as fh:
     fh.write(env.get_template("report.html").render(context))
 
+   
+
 os.system("x-www-browser file://" + os.path.realpath("build/report.html") + " &")
-
-print
-print("Top 5 usernames:")
-results = user_bytes.items()
-results.sort(key = lambda item:item[1], reverse=True)
-for user, transferred_bytes in results[:5]:
-    print user, "==>", transferred_bytes / (1024 * 1024), "MB"
-
-print
-print("Top 5 visited URL-s:")
-results = urls.items()
-results.sort(key = lambda item:item[1], reverse=True)
-for path, hits in results[:5]:
-    print "http://enos.itcollege.ee" + path, "==>", hits, "(", hits * 100 / total, "%)"
-print "The value of __file__ is:", os.path.realpath(__file__)
-print "The directory of __file__ is:", os.path.realpath(os.path.dirname(__file__))
